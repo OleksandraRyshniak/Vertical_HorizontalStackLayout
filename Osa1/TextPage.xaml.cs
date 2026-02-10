4aa8f1ac6a3952f4486eca586ca66e5f5e3def69
opilane <opilane@HP-CZC2349HV2>
2026-02-10 09:00:33 +0200

    new items

--- Osa1/TextPage.xaml.cs
+++ Osa1/TextPage.xaml.cs
@@ -2,8 +2,56 @@ namespace Osa1;
 
 public partial class TextPage : ContentPage
 {
-	public TextPage()
+	Label lbl;
+	Editor editor;
+	HorizontalStackLayout hsl;
+	List<string> nupud = new List<string>() { "Tagasi", "Avaleht", "Edasi" };
+	public TextPage(int i)
 	{
-		InitializeComponent();
+		lbl = new Label
+		{
+			Text = "Pealkiri",
+			FontSize = 36,
+			FontFamily = "Impact",
+			TextColor = Colors.Black,
+			HorizontalOptions = LayoutOptions.Center,
+			FontAttributes = FontAttributes.Bold
+		};
+		editor = new Editor
+		{
+			Placeholder = "Sisesta tekst ...",
+			PlaceholderColor = Colors.Red,
+			FontSize = 17,
+            HorizontalOptions = LayoutOptions.Center,
+            FontAttributes  = FontAttributes.Italic
+		};
+
+		hsl = new HorizontalStackLayout
+		{
+			Spacing = 20,
+			HorizontalOptions = LayoutOptions.Center
+		};
+		for (int j = 0; j < nupud.Count; j++)
+		{
+			Button nupp = new Button
+			{
+				Text = nupud[i],
+				FontSize = 18,
+				FontFamily = "Impact",
+				TextColor = Colors.Black,
+				BackgroundColor = Colors.LightBlue,
+				CornerRadius = 10,
+				HeightRequest = 40,
+				ZIndex = j
+			};
+			hsl.Add(nupp);
+            nupp.Clicked += Liikumine;
+		}
+
 	}
+
+    private void Liikumine(object? sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 }
