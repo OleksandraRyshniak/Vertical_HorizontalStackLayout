39bf74fc60582c26e9379537f028075d6b18a8d0
opilane <opilane@HP-CZC2349HTZ>
2026-02-25 15:44:54 +0200

    end

--- Osa1/RGB_color.xaml.cs
+++ Osa1/RGB_color.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.Maui;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Layouts;
 
@@ -9,6 +10,9 @@ public partial class RGB_color : ContentPage
 	Label lbl, green_lbl, red_lbl, blue_lbl;
 	Slider sl1, sl2, sl3;
 	AbsoluteLayout abs;
+	Button btn;
+    Random rnd = new Random();
+	Stepper stp;
 
     public RGB_color()
 	{
@@ -24,20 +28,38 @@ public partial class RGB_color : ContentPage
 			WidthRequest = 200,
 			HeightRequest = 200
 		};
+		btn = new Button
+		{
+			Text = "Random Color",
+			WidthRequest= 100,
+			HeightRequest = 40
+		};
+        btn.Clicked += Btn_Clicked;
 
-        box1 = Create_box(Colors.Red);
-		box2 = Create_box(Colors.Green);
-		box3 = Create_box(Colors.Blue);
+        stp = new Stepper
+        {
+           Minimum = 0,
+           Maximum = 1,
+           Increment = 0.1,
+           Value = 1,
+           HorizontalOptions = LayoutOptions.Center
+        };
+        stp.ValueChanged += Stp_ValueChanged;
+
+
+        box1 = Create_box();
+		box2 = Create_box();
+		box3 = Create_box();
 
 		sl1 = Create_slider();
-        red_lbl = create_lbl("Red: 255");
+        red_lbl = create_lbl("Red: 0");
         sl2 = Create_slider();
-        green_lbl = create_lbl("Green: 255");
+        green_lbl = create_lbl("Green: 0");
         sl3 = Create_slider();
-		blue_lbl = create_lbl("Blue: 255");
+		blue_lbl = create_lbl("Blue: 0");
 
-        abs = new AbsoluteLayout { Children = { lbl, box1, box2, box3, sl1, red_lbl, sl2, green_lbl, sl3, blue_lbl, color } };
-        List<View> controls = new List<View> { lbl, box1, box2,  box3, sl1, red_lbl, sl2, green_lbl, sl3, blue_lbl, color };
+        abs = new AbsoluteLayout { Children = { lbl, box1, box2, box3, sl1, red_lbl, sl2, green_lbl, sl3, blue_lbl, btn, color, stp } };
+        List<View> controls = new List<View> { lbl, box1, box2,  box3, sl1, red_lbl, sl2, green_lbl, sl3, blue_lbl, btn,  color, stp };
         for (int i = 0; i < controls.Count; i++)
         {
             View v = controls[i];
@@ -57,7 +79,7 @@ public partial class RGB_color : ContentPage
             }
             else 
             {
-                double y = 0.30 + (i - 4) * 0.07;
+                double y = 0.30 + (i - 4) * 0.04;
                 AbsoluteLayout.SetLayoutBounds(v, new Rect(0.5, y, 300, 40));
             }
 
@@ -65,13 +87,24 @@ public partial class RGB_color : ContentPage
         }
         Content = abs;
     }
-    
 
-	private BoxView Create_box(Color color)
+    private void Stp_ValueChanged(object? sender, ValueChangedEventArgs e)
+    {
+        color.Opacity = e.NewValue;
+    }
+
+    private void Btn_Clicked(object? sender, EventArgs e)
+    {
+        sl1.Value = rnd.Next(256);
+        sl2.Value = rnd.Next(256);
+        sl3.Value = rnd.Next(256);
+    }
+
+    private BoxView Create_box()
 	{
 		var box = new BoxView
 		{
-			Color = color,
+			Color = Colors.Black,
 			WidthRequest = 80,
 			HeightRequest = 80,
 			CornerRadius = 20, 
@@ -117,7 +150,11 @@ public partial class RGB_color : ContentPage
             blue_lbl.Text = String.Format("Blue: {0:X2}", (int)e.NewValue);
         }
 
-		color.Color = Color.FromRgb((int)sl1.Value, (int)sl2.Value, (int)sl3.Value);
+		box1.Color = Color.FromRgb((int)sl1.Value, 0, 0);
+        box2.Color = Color.FromRgb(0, (int)sl2.Value, 0); 
+		box3.Color = Color.FromRgb(0, 0, (int)sl3.Value);
+
+        color.Color = Color.FromRgb((int)sl1.Value, (int)sl2.Value, (int)sl3.Value);
 		lbl.TextColor = Color.FromRgb((int)sl1.Value, (int)sl2.Value, (int)sl3.Value);
     }
 }
